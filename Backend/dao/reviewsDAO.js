59a57a58179cf1522f5f17c343e25962d98e9fd9
Yiwen C <chen2362@wisc.edu>
2023-05-28 16:10:57 +0800

    new

---
+++ Backend/dao/reviewsDAO.js
@@ -0,0 +1,82 @@
+import mongodb from "mongodb";
+
+let reviews;
+
+export default class ReviewsDAO {
+  static async injectDB(conn) {
+    if (reviews) {
+      return
+    }
+    try {
+      reviews = await conn.db("reviews").collection("reviews")
+    } catch (e) {
+      console.error(`Unable to establish collection handles in userDAO: ${e}`)
+    }
+  }
+
+  static async addReview(movieId, user, review) {
+    try {
+      const reviewDoc = {
+        movieId: movieId,
+        user: user,
+        review: review,
+      }
+      console.log("adding")
+      return await reviews.insertOne(reviewDoc)
+    } catch (e) {
+      console.error(`Unable to post review: ${e}`)
+      return { error: e }
+    }
+  }
+
+  static async getReview(reviewId) {
+    try {
+      const ObjectId = new mongodb.ObjectId(reviewId) 
+      return await reviews.findOne({ _id: ObjectId })
+    } catch (e) {
+      console.error(`Unable to get review: ${e}`)
+      return { error: e }
+    }
+  }
+
+  static async updateReview(reviewId, user, review) {
+    try {
+      const ObjectId = new mongodb.ObjectId(reviewId)
+      const updateResponse = await reviews.updateOne(
+        { _id: ObjectId },
+        { $set: { user: user, review: review } }
+      )
+
+      return updateResponse
+    } catch (e) {
+      console.error(`Unable to update review: ${e}`)
+      return { error: e }
+    }
+  }
+
+  static async deleteReview(reviewId) {
+
+    try {
+      const ObjectId = new mongodb.ObjectId(reviewId)
+      const deleteResponse = await reviews.deleteOne({
+        _id: ObjectId,
+      })
+
+      return deleteResponse
+    } catch (e) {
+      console.error(`Unable to delete review: ${e}`)
+      return { error: e }
+    }
+  }
+
+  static async getReviewsByMovieId(movieId) {
+    try {
+      const cursor = await reviews.find({ movieId: parseInt(movieId) })
+      return cursor.toArray()
+    } catch (e) {
+      console.error(`Unable to get review: ${e}`)
+      return { error: e }
+    }
+  }
+
+}
